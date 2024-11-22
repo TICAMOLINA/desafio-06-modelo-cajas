2a2894096f250c44dba75e4d40246a1f468c818a
Mauricio Molina <tica.molina57@gmail.com>
2024-11-21 22:34:26 -0300

    Agrego una página nueva que me lleva a una nueva ventana en donde se brindan mas descripciones sobre el juego, en este caso Mario. Con opción de agregar al carrito. Lo hice con multipages vite y le agregué un css personalizado

---
+++ pages/buy_mario/buy_mario.js
@@ -0,0 +1,3 @@
+import '../../style.css'
+
+import './buy_mario.css'
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
+import { resolve } from 'path'
+
 export default {
     css: {
         devSourcemap: true
-    }
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                buy_mario: resolve(__dirname, 'pages/buy_mario/index.html')
+          }
+      }
   }
+}
