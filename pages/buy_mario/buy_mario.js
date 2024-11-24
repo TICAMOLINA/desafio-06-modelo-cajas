3df072e0ac1d0ac104e735f6152adb280dbd4d91
Mauricio Molina <tica.molina57@gmail.com>
2024-11-24 18:10:23 -0300

    Termino de crear los estilos personalizados para las páginas de compra de cada juego

--- pages/buy_mario/buy_mario.js
+++ pages/buy_mario/buy_mario.js
@@ -1,3 +1,7 @@
 import '../../style.css'
 
 import './buy_mario.css'
+
+/* import '../../style-2.css'
+
+import './buy_mario_style2.css' */
--- pages/buy_mortal_kombat/buy_mortal_kombat.js
+++ pages/buy_mortal_kombat/buy_mortal_kombat.js
@@ -1,3 +1,7 @@
 import '../../style.css'
 
 import './buy_mortal_kombat.css'
+
+/* import '../../style-2.css'
+
+import './buy_mortal_kombat_style2.css' */
